e0dd0bf7b44a21ced3400d193f0960f5ebf83773
jdhellfire <jdhellfire@126.com>
2018-02-26 21:09:37 +0800

    仿写python版本的被测代码

---
+++ Account.py
@@ -0,0 +1,36 @@
+class Account:
+    def __init__(self, is_debit):
+        self.__is_debit = is_debit
+        self.__balance = 0
+
+    @staticmethod
+    def open_debit_account():
+        return Account(True)
+
+    @staticmethod
+    def open_account():
+        return Account(False)
+
+    def get_balance(self):
+        pass
+
+    def get_balance(self):
+        return self.__balance
+
+    def deposit(self, amount):
+        self.__balance += amount
+
+    def withdraw(self, amount):
+        insufficient_funds = self.get_balance() < amount
+        self.__balance -= amount
+        if self.__is_debit and insufficient_funds:
+            raise InsufficientFundsException()
+
+
+class InsufficientFundsException(Exception):
+    def __init__(self):
+        super().__init__(self)
+
+    def __str__(self):
+        return 'Account Exception'
+
